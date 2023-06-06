aa3d7333f572eaecc7523096ab5481c091ca77bf
ErickRen <ErickRen2022@outlook.com>
2023-06-06 16:58:27 +0800

    todo: Https SSLSocket.

--- impl/src/main/java/me/erickren/Main.java
+++ impl/src/main/java/me/erickren/Main.java
@@ -1,14 +1,39 @@
 package me.erickren;
 
-import me.erickren.request.HttpRequest;
-import me.erickren.request.HttpRequestImpl;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        HttpRequest httpRequest = new HttpRequestImpl("https://zhuanlan.zhihu.com/p/113444636");
-        httpRequest.get();
+//        HttpRequest httpRequest = new HttpRequestImpl("https://juejin.cn/");
+//        httpRequest.get();
 
+        String host = "juejin.cn";
+        int port = 443;
+        String request = "GET / HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" +
+                "Connection: close\r\n\r\n";
+
+        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket socket = (SSLSocket) factory.createSocket(host, port);
+        socket.startHandshake();
+
+        OutputStream os = socket.getOutputStream();
+        os.write(request.getBytes());
+        os.flush();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+        }
+        in.close();
+        socket.close();
     }
+
 }
